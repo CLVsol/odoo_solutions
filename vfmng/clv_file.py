77e271d3cde29865f555ccab6661366f2747fce0
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2014-09-08 14:51:12 -0300

    Updated solution vfmng.

--- vfmng/clv_file.py
+++ vfmng/clv_file.py
@@ -17,4 +17,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ################################################################################
 
-import clv_file
+from openerp import models, fields, api
+
+class clv_file(models.Model):
+    _inherit = 'clv_file'
+
+    _defaults = {
+        'active_history': True, 
+        }
